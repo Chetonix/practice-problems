d7b785dbe5821393f64b6ea1618f88a53375d679
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-06-06 14:53:53 +0530

    add alternate sol to the problem2 LinkedList exercise

--- LinkedList/problem2.js
+++ LinkedList/problem2.js
@@ -1,22 +1,39 @@
 // Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
 
-var getIntersectionNode = function(headA, headB) {
-    let listA = headA;
-    let listB = headB;
-    let arrA = [];
-    // let arrB = [];
+// var getIntersectionNode = function(headA, headB) {
+//     let listA = headA;
+//     let listB = headB;
+//     let arrA = [];
+//     // let arrB = [];
     
-    while(listA) {
-        arrA.push(listA);
-        listA = listA.next;
+//     while(listA) {
+//         arrA.push(listA);
+//         listA = listA.next;
         
-    }
+//     }
     
-    while(listB) {
-        if (arrA.includes(listB)) {
-            return listB;
-        }
-        listB = listB.next;
+//     while(listB) {
+//         if (arrA.includes(listB)) {
+//             return listB;
+//         }
+//         listB = listB.next;
+//     }
+//     return null;
+// };
+
+//An alternate solution
+var getIntersectionNode = function(headA, headB) {
+
+    let nodeRefA = headA;
+    let nodeRefB = headB;
+
+    while(nodeRefA !== nodeRefB) {
+        nodeRefA = nodeRefA === null ? headB : nodeRefA.next;
+        nodeRefB = nodeRefB === null ? headA : nodeRefB.next;
     }
-    return null;
-};
+
+    return nodeRefA;
+    
+    
+
+}
