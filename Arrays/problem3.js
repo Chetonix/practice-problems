03d5b0532583496ebce1a6c4a51807424495fcd1
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-06-06 12:43:36 +0530

    add another exactly similar sol to the sqrt exercise

--- Arrays/problem3.js
+++ Arrays/problem3.js
@@ -39,4 +39,24 @@ var mySqrt = function (x) {
     
 // };
 
+
+//A simpler version (just sayin!) 
+// var mySqrt = function(x) {
+//     let start = 0;
+//     let end = x;
+    
+//     while(start<=end) {
+//         let mid = Math.floor((start + end) / 2);
+//         if (mid * mid === x) {
+//             return mid;
+//         }else if (mid*mid > x) {
+//             end = mid -1;
+//         } else if (mid*mid < x) {
+//             start = mid + 1;
+
+//         }
+//     }
+//     return start-1;
+// };
+
 console.log(mySqrt(8));
