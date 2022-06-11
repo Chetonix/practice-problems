38ef2be629e30be8900148919cd9cc0308d0e0d6
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-06-11 13:21:33 +0530

    add a iterative sol to the 3rd problem of Stacks folder"

--- Stacks/problem3.js
+++ Stacks/problem3.js
@@ -1,19 +1,50 @@
 // Binary Tree Postorder Traversal
 
+// Recursive Solution //
+// var postorderTraversal = function(root) {
+//     const arr = [];
+//     function traverse (x) {
+//         if (!x) {
+//             return;
+//         }
+        
+//         traverse(x.left);
+//         traverse(x.right);
+//         arr.push(x.val);
+//     }
+//    traverse(root);
+    
+//     return arr;
+    
+// };
 
 var postorderTraversal = function(root) {
-    const arr = [];
-    function traverse (x) {
-        if (!x) {
-            return;
+    let firstStack = [root];
+    let secondStack = [];
+    let sol = [];
+    
+    while(firstStack.length > 0) {
+        
+        if (root === null) {
+            return [];
         }
         
-        traverse(x.left);
-        traverse(x.right);
-        arr.push(x.val);
+
+        let temp = firstStack.pop();
+        
+        secondStack.push(temp);
+        if(temp.left !==null) {
+            firstStack.push(temp.left);
+        }
+        if (temp.right !== null) {
+             firstStack.push(temp.right);
+        }
+       
     }
-   traverse(root);
     
-    return arr;
+    while(secondStack.length > 0) {
+        sol.push(secondStack.pop().val);
+    }
     
-};
+    return sol;
+}
